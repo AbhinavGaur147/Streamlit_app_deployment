e84b5776e19231d8390f02162e258b203942e440
AbhinavGaur147 <92721755+AbhinavGaur147@users.noreply.github.com>
2024-12-14 14:48:46 +0530

    Update Streanlit_app.py

--- Streanlit_app.py
+++ Streanlit_app.py
@@ -29,3 +29,9 @@ st.write(f"You selected: {option}")
 name = st.text_input("Enter your name")
 if name:
     st.write(f"Hello, {name}!")
+
+# URL of the image
+image_url = "https://media.istockphoto.com/id/523761634/photo/cute-panda-bear-climbing-in-tree.jpg?s=612x612&w=0&k=20&c=TxsmORsbuY1LpxQsc6T8fpWJo7lBwncciYhroAr8rXI="
+
+# Display the image
+st.image(image_url, caption='Sample Image', use_column_width=True)
